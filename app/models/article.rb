62d7d4744033675db5b9ad4a8ec2d97a362d2138
domanhtien2011 <domanhtien2011@gmail.com>
2016-02-25 23:07:02 +0700

    Add styling to show template

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 5}
-  validates :body, presence: true
+  validates :body, presence: true, length: { minimum: 10}
 end
