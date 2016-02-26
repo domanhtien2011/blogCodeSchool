5b0aac52a3f7a2e639986f9680b8c9537a7afe08
domanhtien2011 <domanhtien2011@gmail.com>
2016-02-27 00:00:59 +0700

    Add toggle to navbar

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
+  include PgSearch
+  pg_search_scope :search, against: [:name]
   validates :title, presence: true, length: { minimum: 5}
   validates :body, presence: true, length: { minimum: 10}
   has_many :comments, dependent: :destroy
